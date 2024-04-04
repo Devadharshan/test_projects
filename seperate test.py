86e682e357df4fcce38e41d7b043eb9bad735640
Devadharshan <59635817+Devadharshan@users.noreply.github.com>
2024-04-04 13:13:26 +0530

    Update seperate test.py

--- seperate test.py
+++ seperate test.py
@@ -43,3 +43,24 @@ with open("your_text_file.txt", "r") as file:
             # Split the server name and database name based on "."
             server, db = server_pair.split(".")
             print("Server:", server, "DB:", db)
+
+
+
+# Open the text file
+with open("your_text_file.txt", "r") as file:
+    # Read each line
+    for line in file:
+        # Split the line based on the "|" delimiter to separate master server from replicas
+        master_server, replicas = line.strip().split(" | ")
+        
+        # Split the master server to get server name and database
+        master_server_name, master_db = master_server.split(".")
+        print("Master Server:", master_server_name, "DB:", master_db)
+        
+        # Split the replicas by ","
+        replica_servers = replicas.split(",")
+        # Iterate over each replica
+        for replica_server in replica_servers:
+            # Split the replica server to get server name and database
+            replica_server_name, replica_db = replica_server.split(".")
+            print("Replica Server:", replica_server_name, "DB:", replica_db)
