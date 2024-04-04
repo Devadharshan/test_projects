cdedac4d1991b456bdd741bfb3d5e89d2fdf1c14
Devadharshan <59635817+Devadharshan@users.noreply.github.com>
2024-04-04 12:50:42 +0530

    Update

---
+++ seperate test.py
@@ -0,0 +1,11 @@
+# Open the text file
+with open("your_text_file.txt", "r") as file:
+    # Read each line
+    for line in file:
+        # Split the line based on the "|" delimiter
+        servers = line.strip().split(" | ")
+        # Iterate over each server in the line
+        for server in servers:
+            # Split the server name and database name based on "."
+            server_name, db_name = server.split(".")
+            print("Server:", server_name, "DB:", db_name)
