a928a6c5928af4f5ff15abb5b601e4417c795f3f
Devadharshan <59635817+Devadharshan@users.noreply.github.com>
2024-04-04 13:00:09 +0530

    Update seperate test.py

--- seperate test.py
+++ seperate test.py
@@ -28,3 +28,18 @@ with open("your_text_file.txt", "r") as file:
                 print("Server:", server_name, "DB:", db_name)
             except ValueError:
                 print("Error: Malformed line -", server)
+
+
+
+
+# Open the text file
+with open("your_text_file.txt", "r") as file:
+    # Read each line
+    for line in file:
+        # Split the line based on the "|" delimiter
+        server_pairs = line.strip().split(" | ")
+        # Iterate over each pair of servers
+        for server_pair in server_pairs:
+            # Split the server name and database name based on "."
+            server, db = server_pair.split(".")
+            print("Server:", server, "DB:", db)
